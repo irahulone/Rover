96b5f260d616dda3167eada8eabef2659867533b
vboxuser <vboxuser@ubuntu22.myguest.virtualbox.org>
2023-05-03 14:20:59 -0700

    Added updated workspace.

---
+++ rover_ws_ri/build/locomotion_core/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/vboxuser/rover_ws_ri/install/locomotion_core'
--- rsl_ros2_ws/build/locomotion_core/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/vboxuser/rsl_ros2_ws/install/locomotion_core'
